3003ddb9c2dae8f08cf40cd2b48a81bea8f20349
Steve Ropa <steve@rmprogrammers.com>
2020-03-17 12:52:33 -0600

    first commit

---
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,41 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+    private Item[] items;
+
+    @BeforeEach
+    void setUp() {
+        items = new Item[1];
+    }
+
+    @Test
+    void testCakeName() {
+        items[0] = new Item("Conjured Mana Cake", 1, 2) ;
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Conjured Mana Cake", app.items[0].name);
+    }
+
+    @Test
+    public void testCakeSellin() {
+        items[0] = new Item("Conjured Mana Cake", 1, 2);
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0,app.items[0].sellIn);
+    }
+
+    @Test
+    public void testCakeQuality() {
+        items[0] = new Item("Conjured Mana Cake", 1, 4);
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(2, app.items[0].quality);
+    }
+
+}
